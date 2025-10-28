824b0a76e82f094b577b4ff3763eb7cb22ecd916
이강욱 <kangwook.yi@gmail.com>
2025-10-28 16:50:06 +0900

    터치 로직 변경. 이제 기기의 터치 정보를 직접 받아오도록 함.
    TouchReceiver 오브젝트 불필요.(현재 꺼놨음)
    터치 시작점에서 UI오브젝트가 검출될 경우 조작 불가하도록 변경.
    터치 시작점이 가상 조이스틱 범위 내였을 경우 바로 조작되도록 변경.

--- Assets/_Proj/Scripts/Player/Joystick.cs
+++ Assets/_Proj/Scripts/Player/Joystick.cs
@@ -1,7 +1,8 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 using UnityEngine.UI;
-
 public class Joystick : MonoBehaviour
 {
     [SerializeField] private Image bg;
@@ -15,6 +16,8 @@ public class Joystick : MonoBehaviour
     // 조이스틱 터치 위치 계산에 사용
     private RectTransform rectTransform;
 
+    
+
     void Start()
     {
         rectTransform = GetComponent<RectTransform>();
@@ -22,8 +25,89 @@ public class Joystick : MonoBehaviour
         InputDir = Vector3.zero;
     }
 
+    void Update()
+    {
+        //#if UNITY_EDITOR
+        //        Touch touch = new();
+        //        Vector2 pos = UnityEngine.InputSystem.Mouse.current.position.ReadValue();
+        //        if (Mouse.current.leftButton.wasPressedThisFrame)
+        //        {
+        //            touch.phase = TouchPhase.Began;
+        //            touch.position = pos;
+
+        //        }
+        //        if (Mouse.current.leftButton.isPressed)
+        //        {
+        //            touch.phase = TouchPhase.Moved;
+        //            touch.position = pos;
+        //        }
+        //        if (Mouse.current.leftButton.wasReleasedThisFrame)
+        //        {
+        //            touch.phase = TouchPhase.Ended;
+        //            touch.position = pos;
+        //        }
+
+        //        if (touch.phase == TouchPhase.Began) //터치가 시작될 때
+        //        {
+        //            //가상 조이스틱 전체를 터치 위치로 옮겨야 함.
+        //            transform.position = pos;
+        //            Drag(pos);
+        //        }
+        //        else if (touch.phase <= TouchPhase.Stationary) //를 제외하고 터치가 이동중/정지중일 때
+        //        {
+        //            //가상 조이스틱 핸들을 터치 위치로 옮기되, 범위를 넘어가지 않게 하면 됨.
+        //            Drag(pos);
+        //        }
+        //        else //까지 제외하고 터치가 종료/취소될 때
+        //        {
+        //            ResetJoystick();
+        //        }
+
+        //#elif UNITY_ANDROID
+        //if (Touchscreen.current == null || Touchscreen.current.touches.Count < 1)
+        //{
+        //    Debug.Log("터치 없음");
+        //    return;
+        //}
+
+        var touch = Touchscreen.current.touches[0];
+        //터치 위치로 레이를 쏴서, UI요소가 있으면 리턴시키기 근데이제 조이스틱 자체는 제외해줘야 함
+        if (touch.press.wasReleasedThisFrame)//터치가 종료/취소될 때
+        {
+            ResetJoystick();
+        }
+        if (touch.press.isPressed) //를 제외하고 터치가 이동중/정지중일 때
+        {
+            //터치 시작 위치로 레이를 쏴서, UI요소가 하나라도 검출되면 리턴
+            List<RaycastResult> results = new();
+            
+            PointerEventData data = new(EventSystem.current);
+            data.position = touch.startPosition.ReadValue();
+            EventSystem.current.RaycastAll(data, results);
+            foreach (var r in results)
+            {
+                if (r.gameObject)
+                {
+                    return;
+                }
+            }
+
+            transform.position = Vector2.Distance(transform.position, touch.startPosition.ReadValue()) < 150 ? transform.position : touch.startPosition.ReadValue();
+
+            //여기까지 왔다면, 터치 시작 위치가 UI요소 위가 아닌 것임.
+            //transform.position = touch.startPosition.ReadValue();
+
+            //가상 조이스틱 핸들을 터치 위치로 옮기되, 범위를 넘어가지 않게 하면 됨.
+            Drag((Vector3)touch.position.ReadValue() - transform.position);
+        }
+        
+       
+
+    }
+
     public void MoveToTouch(PointerEventData eventData)
     {
+        
         Vector2 pos;
         
         var parent = rectTransform.parent as RectTransform;
@@ -40,6 +124,21 @@ public class Joystick : MonoBehaviour
         RectTransform bgRect = bg.rectTransform;
         Vector2 pos;
         if (!RectTransformUtility.ScreenPointToLocalPointInRectangle(bgRect, eventData.position, eventData.pressEventCamera, out pos)) return;
+
+        // 핸들 이동반경 제한
+        Vector2 clamped = Vector2.ClampMagnitude(pos, moveRange);
+        handle.rectTransform.anchoredPosition = clamped;
+
+        Vector2 inputNormal = clamped.sqrMagnitude > 0.0001f ? (clamped / moveRange) : Vector2.zero;
+        InputDir = new Vector3(inputNormal.x, 0, inputNormal.y);
+    }
+
+    //HACK: 1028 - 강욱: Drag()메서드 오버로드합니다. Vector2를 매개변수로 갖게 하여 터치된 위치를 기준으로 작동하도록 합니다.
+    public void Drag(Vector2 pos)
+    {
+        //RectTransform bgRect = bg.rectTransform;
+        //Vector2 pos;
+        //if (!RectTransformUtility.ScreenPointToLocalPointInRectangle(bgRect, eventData.position, eventData.pressEventCamera, out pos)) return;
 
         // 핸들 이동반경 제한
         Vector2 clamped = Vector2.ClampMagnitude(pos, moveRange);
