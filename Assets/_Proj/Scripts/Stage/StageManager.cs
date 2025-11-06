508b6cd506fd4385a444f62ee41455c718454dd8
JangWh <janghy95@naver.com>
2025-11-06 17:33:17 +0900

    StageManager에 메서드 이름 다른것 변경

--- Assets/_Proj/Scripts/Stage/StageManager.cs
+++ Assets/_Proj/Scripts/Stage/StageManager.cs
@@ -189,7 +189,7 @@ public class StageManager : MonoBehaviour
             if (block is IEdgeColliderHandler handlerBlock)
             {
                 handlerBlock.Inject();
-                //handlerBlock.Inspect();
+                handlerBlock.DetectAndApplyFourEdge();
             }
         }
     }
